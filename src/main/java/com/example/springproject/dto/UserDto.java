3cea56fbac79dc7ce8816bcbce0fdee43f743cd7
shanukavinda <kshanuka97@gmail.com>
2023-09-28 21:33:33 +0530

    added maven dependency model mapping, added UserService and UserRepo packages and related java classes.

--- src/main/java/com/example/springproject/dto/UserDto.java
+++ src/main/java/com/example/springproject/dto/UserDto.java
@@ -1,4 +1,14 @@
 package com.example.springproject.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class UserDto {
+    private int id;
+    private String name;
+    private String address;
 }
---
+++ src/main/java/com/example/springproject/service/UserService.java
@@ -0,0 +1,23 @@
+package com.example.springproject.service;
+
+import com.example.springproject.dto.UserDto;
+import com.example.springproject.entity.User;
+import com.example.springproject.repo.UserRepo;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class UserService {
+    @Autowired
+    private UserRepo userRepo;
+    @Autowired
+    private ModelMapper modelMapper;
+    public UserDto saveUser(UserDto userDto){
+        userRepo.save(modelMapper.map(userDto, User.class));
+        return userDto;
+    }
+
+}
