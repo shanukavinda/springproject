89053afb94ea9ff441889e8cf096dda8ccad9001
shanukavinda <kshanuka97@gmail.com>
2023-09-28 12:23:41 +0530

    added few comments

--- src/main/java/com/example/springproject/entity/User.java
+++ src/main/java/com/example/springproject/entity/User.java
@@ -7,11 +7,11 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
+@AllArgsConstructor //constructor with all parameters
+@NoArgsConstructor //default constructor
+@Data //getters and setters
 public class User {
-    @Id
+    @Id //annotates the primary id
     private int id;
     private String name;
     private String address;
